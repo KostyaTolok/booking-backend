a0061ecb279933d21aa05f93b966a0972b366462
Kostya <tolok.kostya@gmail.com>
2022-10-02 23:24:30 +0300

    add get_user_id function

--- Search/rooms/permissions.py
+++ Search/rooms/permissions.py
@@ -1,21 +1,16 @@
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import BasePermission
 
-from common.utils import decode_token
+from common.utils import get_user_id
 
 
 class IsRoomOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         jwt_token = request.headers.get("Authorization", None)
 
-        if not jwt_token:
+        try:
+            user_id = get_user_id(jwt_token)
+        except AuthenticationFailed:
             return False
-
-        payload = decode_token(jwt_token)
-        user_id = payload.get("sub", None)
-
-        if not user_id.isdigit():
-            return False
-
-        user_id = int(user_id)
 
         return user_id == obj.hotel.owner
