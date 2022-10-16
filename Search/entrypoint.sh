b20868ec47e6ed3ba833428228a0771ab3f7fb04
Kostya <tolok.kostya@gmail.com>
2022-10-16 16:29:46 +0300

    add fixtures, minor views, serializers and models fixes

--- Search/entrypoint.sh
+++ Search/entrypoint.sh
@@ -3,5 +3,11 @@
 echo "Applying database migrations"
 python manage.py migrate
 
+python manage.py loaddata cities
+python manage.py loaddata hotels
+python manage.py loaddata hotel_images
+python manage.py loaddata rooms
+python manage.py loaddata room_images
+
 echo "Starting server on 0.0.0.0:3000"
 python manage.py runserver 0.0.0.0:3000
