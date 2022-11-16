60ad15774082536e22f7ebc7a0d3ce9b588bd458
Kostya <tolok.kostya@gmail.com>
2022-11-16 18:04:04 +0300

    Fix html template

--- Users/app/services/email.py
+++ Users/app/services/email.py
@@ -5,22 +5,12 @@ from app.core.config import config
 async def send_reset_password_email(email: str, token: str) -> None:
     message = {
         "email": email,
-        "subject": "",
+        "subject": "PandaHouse password reset",
         "html": f"""
-        <style>
-            .button {{
-              background-color: #E6A064;
-              border-radius: 5px;
-              color: white;
-              padding: .5em;
-              text-decoration: none;
-            }}
-        </style>
-        <h2>PandaHouse password reset</h2>
         <p>
             Hi! We heard that you lost your PandaHouse password. Sorry about that!
         </p>
-        <a class="button" href="https://{config.HOST}/booking-mobile/reset-password/confirm/{token}">Reset Your Password</a>
+        <a href="https://{config.HOST}/booking-mobile/reset-password/confirm/{token}">Reset Your Password</a>
         """,
     }
     await send_email(**message)
