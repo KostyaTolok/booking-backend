7de0367a4ea9b417e6adc3b4993e90d7059ccd62
Kostya <tolok.kostya@gmail.com>
2022-11-21 19:03:31 +0300

    Move creation of exchange to events

--- Payments/app/core/amqp_connection.py
+++ Payments/app/core/amqp_connection.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from aio_pika import connect, ExchangeType, Exchange
+from aio_pika import connect, ExchangeType
 from aio_pika.abc import AbstractConnection
 
 from app.core.config import config
@@ -8,24 +8,12 @@ from app.core.config import config
 
 class SingletonAmqp:
     amqp_connection: Optional[AbstractConnection] = None
-    exchange: Optional[Exchange] = None
 
     @classmethod
     async def get_amqp_connection(cls) -> AbstractConnection:
         if cls.amqp_connection is None:
             cls.amqp_connection = await connect(config.RABBITMQ_URL)
         return cls.amqp_connection
-
-    @classmethod
-    async def get_payments_exchange(cls):
-        if cls.exchange is None:
-            connection = await cls.get_amqp_connection()
-            channel = await connection.channel()
-            cls.exchange = await channel.declare_exchange(
-                config.RABBITMQ_PAYMENT_EVENTS_EXCHANGE_NAME,
-                ExchangeType.FANOUT,
-            )
-        return cls.exchange
 
     @classmethod
     async def close_amqp_connection(cls) -> None:
--- Payments/app/events.py
+++ Payments/app/events.py
@@ -3,10 +3,15 @@ import json
 from aio_pika import Message, ExchangeType, DeliveryMode
 
 from app.core.amqp_connection import SingletonAmqp
+from app.core.config import config
 
 
 async def send_payment_event(message: dict):
-    exchange = await SingletonAmqp.get_payments_exchange()
+    connection = await SingletonAmqp.get_amqp_connection()
+    channel = await connection.channel()
+    exchange = await channel.get_exchange(
+        config.RABBITMQ_PAYMENT_EVENTS_EXCHANGE_NAME, ensure=True
+    )
 
     message = Message(
         json.dumps(message, default=str).encode("utf-8"),
@@ -14,3 +19,12 @@ async def send_payment_event(message: dict):
     )
 
     await exchange.publish(message, routing_key="payment")
+
+
+async def create_payments_exchange():
+    connection = await SingletonAmqp.get_amqp_connection()
+    channel = await connection.channel()
+    await channel.declare_exchange(
+        config.RABBITMQ_PAYMENT_EVENTS_EXCHANGE_NAME,
+        ExchangeType.FANOUT,
+    )
