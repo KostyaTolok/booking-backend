67e5358ab6e0fdbc62ceb66c932c95a3d06c1f34
Kostya <tolok.kostya@gmail.com>
2022-11-24 22:40:56 +0300

    Merge main

--- Search/booking/views.py
+++ Search/booking/views.py
@@ -1,6 +1,5 @@
-from rest_framework import mixins, viewsets, status
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from booking.models import Booking
