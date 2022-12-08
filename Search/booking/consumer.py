bd63dec9f8a261be17378799cd2535334f661f5e
Kostya <tolok.kostya@gmail.com>
2022-12-08 23:14:52 +0300

    Add retrying of running consumer

--- Search/booking/consumer.py
+++ Search/booking/consumer.py
@@ -3,6 +3,7 @@ import logging
 import threading
 
 import pika
+from retrying import retry
 
 
 class Consumer(threading.Thread):
@@ -11,25 +12,30 @@ class Consumer(threading.Thread):
         self.logger = logging.getLogger(__name__)
         super().__init__()
 
+    @retry(stop_max_attempt_number=5, wait_fixed=2000)
     def run(self):
-        parameters = pika.URLParameters(self.settings.BOOKING_BROKER_URL)
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        channel.queue_declare(
-            self.settings.BOOKING_QUEUE_NAME,
-            durable=True,
-        )
-        channel.queue_bind(
-            exchange=self.settings.BOOKING_EXCHANGE_NAME,
-            queue=self.settings.BOOKING_QUEUE_NAME,
-        )
-        channel.basic_qos(prefetch_count=10)
-        channel.basic_consume(
-            on_message_callback=self.process_message,
-            queue=self.settings.BOOKING_QUEUE_NAME,
-            auto_ack=True,
-        )
-        channel.start_consuming()
+        try:
+            parameters = pika.URLParameters(self.settings.BOOKING_BROKER_URL)
+            connection = pika.BlockingConnection(parameters)
+            channel = connection.channel()
+            channel.queue_declare(
+                self.settings.BOOKING_QUEUE_NAME,
+                durable=True,
+            )
+            channel.exchange_declare(exchange=self.settings.BOOKING_EXCHANGE_NAME, exchange_type="fanout", passive=True)
+            channel.queue_bind(
+                exchange=self.settings.BOOKING_EXCHANGE_NAME,
+                queue=self.settings.BOOKING_QUEUE_NAME,
+            )
+            channel.basic_qos(prefetch_count=10)
+            channel.basic_consume(
+                on_message_callback=self.process_message,
+                queue=self.settings.BOOKING_QUEUE_NAME,
+                auto_ack=True,
+            )
+            channel.start_consuming()
+        except Exception as e:
+            self.logger.error(e)
 
     def process_message(self, channel, method, properties, body):
         from booking.models import Booking
